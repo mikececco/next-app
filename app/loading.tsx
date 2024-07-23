481d2a67376b1b74d7f6bb47a45225ba617618e8
Mike Cecconello <mikececco2000@gmail.com>
2024-07-23 11:26:27 +0200

    Loading file (replaces Suspense automatically)

---
+++ app/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div>
+      <span className='loading loading-spinner loading-sm'>
+      </span>
+    </div>
+  )
+}
+
+export default Loading
